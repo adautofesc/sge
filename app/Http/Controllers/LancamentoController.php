308887925f1c5d1caa0d7fed709470db2ad9db65
alunoadauto <aluno.adauto@gmail.com>
2018-02-15 16:41:26 -0200

    new file:   app/Http/Controllers/LancamentoController.php 	new file:   app/Lancamento.php

---
+++ app/Http/Controllers/LancamentoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Matricula;
+use App\Boleto;
+use App\Lancamento;
+
+
+class LancamentoController extends Controller
+{
+    //
+	public function gerarLancamentos($parcela){
+		$matriculas=Matricula::where('status','ativa')->where('valor','>',0)->where('parcelas','>=',$parcela)->get();
+		foreach($matriculas as $matricula){
+			if(!$this->verificaSeLancada($matricula->id,$parcela)){
+				$lancamento=new Lancamento;
+				$lancamento->matricula=$matricula->id;
+				$lancamento->parcela=$parcela;
+				$lancamento->valor=($matricula->valor-$matricula->valor_desconto)/$matricula->parcelas;
+				if($lancamento->valor>0)
+					$lancamento->save();
+			}
+		}
+		//gerar os boletos.
+		return "lancamentos efetuados com sucesso";
+
+	}
+
+	public function verificaSeLancada($matricula,$parcela){
+		$lancamentos=Lancamento::where('matricula',$matricula)->where('parcela',$parcela)->get();
+		if (count($lancamentos)>0)
+			return true;
+		else
+			return false;
+	}
+
+
+}

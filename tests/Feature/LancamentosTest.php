1b05d890175d9ec1d614605780717e3a29786d08
alunoadauto <aluno.adauto@gmail.com>
2018-07-06 11:55:40 -0300

    teste lancamentos

--- tests/Feature/LancamentosTest.php
+++ tests/Feature/LancamentosTest.php
@@ -40,110 +40,112 @@ class LancamentosTest extends TestCase
         $erros = 0;
         $erro_str='';
 
-        //Amostra 1: matrícula ativa UATI 1 disciplina
-        $matricula1 = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
+        //lista de cursos e status que serão testados
+        $cursos = array('307','898','1151','1432','1042'); //uati, hidro, natação, tecnologias, smartphone, corte/costura
+        $status = array('ativa','pendente','espera');
+        // nº de disciplinas realizado com for.
+        // 
+        foreach($cursos as $curso){
+            foreach($status as $estado){
+                for($i=1;$i<6;$i++){
+
+                    //*******************************
+                    //sem bolsa
+                    //*******************************
+                    $matricula = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
                         ->selectRaw('matriculas.*, count(inscricoes.id) as numero_inscricoes')
-                        ->where('matriculas.curso',307)
+                        ->where('matriculas.curso',$curso)
                         ->where('matriculas.status','ativa')
                         ->where('inscricoes.status','regular')
                         ->groupBy('matriculas.id')
-                        ->havingRaw('count(inscricoes.id) = ?',[1])
+                        ->havingRaw('count(inscricoes.id) = ?',[$i])
                         ->inRandomOrder()
                         ->first();
-        $amostra1 = \App\Lancamento::where('matricula', $matricula1->id)->orderbyDesc('parcela')->first();
-        if($amostra1->parcela >5 || abs($amostra1->valor - 20.00) != 00){
-            $erros++;
-            $erro_str = $erro_str . 'Lançamento '.$amostra1->id.' da pessoa '.$amostra1->pessoa. ': Parcelas: '.$amostra1->parcela.'/5 Valor:'.$amostra1->valor.'/20'."\n";
-        }
+                    if($matricula){
+                        $amostra = \App\Lancamento::where('matricula', $matricula->id)->orderbyDesc('parcela')->first();
+                        $inscricao = \App\Inscricao::where('matricula',$matricula->id)->first();
+                    }
+                    if($inscricao)
+                        $turma = \App\Turma::find($inscricao->turma->id);
 
-        //Amostra 2: matrícula ativa UATI 2 disciplina
-        $matricula2 = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
+                   
+
+                    if($curso == 307){   
+                        if($i==1)
+                            $valor = 20;
+                        if($i>1 && $i<5)
+                            $valor = 50;
+                        if($i>5)
+                            $valor = 80;
+                    }
+                    else{
+                        $valor = str_replace(',', '.',$turma->valor );
+                    }
+
+                    if($curso == 898 || $curso == 1151)
+                        $parcelas = 11;
+                    else
+                        $parcelas = 5;
+
+
+                    if($amostra){
+                        if($amostra->parcela > $parcelas || abs($amostra->valor - $valor) != 00){
+                        $erros++;
+                        $erro_str = $erro_str . 'Lançamento '.$amostra->id.' da pessoa '.$amostra->pessoa. ': Parcelas: '.$amostra->parcela.'/'.$parcelas .' Valor:'.$amostra->valor.'/'.$valor."\n";
+                        }
+                    }
+                    
+                    
+                    //******************************
+                    //com bolsa
+                    //******************************
+                    
+                    $matricula = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
                         ->selectRaw('matriculas.*, count(inscricoes.id) as numero_inscricoes')
-                        ->where('matriculas.curso',307)
+                        ->where('matriculas.curso',$curso)
                         ->where('matriculas.status','ativa')
                         ->where('inscricoes.status','regular')
+                        ->where('matriculas.desconto','>',0)
                         ->groupBy('matriculas.id')
-                        ->havingRaw('count(inscricoes.id) = ?',[2])
+                        ->havingRaw('count(inscricoes.id) = ?',[$i])
                         ->inRandomOrder()
                         ->first();
-        $amostra2 = \App\Lancamento::where('matricula', $matricula2->id)->orderbyDesc('parcela')->first();
-        if($amostra2->parcela >5 || abs($amostra2->valor - 50.00) != 00){
-            $erros++;
-            $erro_str = $erro_str . 'Lançamento '.$amostra2->id.' da pessoa '.$amostra2->pessoa. ': Parcelas: '.$amostra2->parcela.'/5 Valor:'.$amostra2->valor.'/50'."\n";
-        }
+                    if($matricula){
+                        $amostra = \App\Lancamento::where('matricula', $matricula->id)->orderbyDesc('parcela')->first();
+                        $inscricao = \App\Inscricao::where('matricula', $matricula->id)->first();
+                    }
+                    if($inscricao)
+                        $turma = \App\Turma::find($inscricao->turma->id);
 
+                    
 
-        
+                    if($curso == 307){   
+                        if($i==1)
+                            $valor = 20;
+                        if($i>1 && $i<5)
+                            $valor = 50;
+                        if($i>5)
+                            $valor = 80;
+                    }
+                    else{
+                        $valor = str_replace(',', '.',$turma->valor );
+                    }
 
-        //Amostra 3: matrícula ativa UATI 3 disciplinas
-        $matricula3 = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
-                        ->selectRaw('matriculas.*, count(inscricoes.id) as numero_inscricoes')
-                        ->where('matriculas.curso',307)
-                        ->where('matriculas.status','ativa')
-                        ->where('inscricoes.status','regular')
-                        ->groupBy('matriculas.id')
-                        ->havingRaw('count(inscricoes.id) = ?',[3])
-                        ->inRandomOrder()
-                        ->first();
-        $amostra3 = \App\Lancamento::where('matricula', $matricula3->id)->orderbyDesc('parcela')->first();
-        if($amostra3->parcela >5 || abs($amostra3->valor - 50.00) != 00){
-            $erros++;
-            $erro_str = $erro_str . 'Lançamento '.$amostra3->id.' da pessoa '.$amostra3->pessoa. ': Parcelas: '.$amostra3->parcela.'/5 Valor:'.$amostra3->valor.'/50'."\n";
-        }
+                    if($curso == 898 || $curso == 1151)
+                        $parcelas = 11;
+                    else
+                        $parcelas = 5;
 
+                    if($amostra){
+                        if($amostra->parcela > $parcelas || abs($amostra->valor - $valor) != 00){
+                        $erros++;
+                        $erro_str = $erro_str . 'Lançamento '.$amostra->id.' da pessoa '.$amostra->pessoa. ': Parcelas: '.$amostra->parcela.'/'.$parcelas .' Valor:'.$amostra->valor.'/'.$valor."\n";
+                        }
+                    }
+                }// end for
+            }// end foreach status
+        }// end foreach cursos
 
-        //Amostra 4: matrícula ativa UATI 5 disciplinas
-        $matricula4 = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
-                        ->selectRaw('matriculas.*, count(inscricoes.id) as numero_inscricoes')
-                        ->where('matriculas.curso',307)
-                        ->where('matriculas.status','ativa')
-                        ->where('inscricoes.status','regular')
-                        ->groupBy('matriculas.id')
-                        ->havingRaw('count(inscricoes.id) = ?',[5])
-                        ->inRandomOrder()
-                        ->first();
-        $amostra4 = \App\Lancamento::where('matricula', $matricula4->id)->orderbyDesc('parcela')->first();
-        if($amostra4->parcela >5 || abs($amostra4->valor - 80.00) != 00){
-            $erros++;
-            $erro_str = $erro_str . 'Lançamento '.$amostra4->id.' da pessoa '.$amostra4->pessoa. ': Parcelas: '.$amostra4->parcela.'/5 Valor:'.$amostra2->valor.'/80'."\n";
-        }
-
-        //Amostra 5: matrícula pendente UATI 1 disciplina
-        $matricula5 = \App\Matricula::leftjoin('inscricoes','matriculas.id', '=','inscricoes.matricula' )
-                        ->selectRaw('matriculas.*, count(inscricoes.id) as numero_inscricoes')
-                        ->where('matriculas.curso',307)
-                        ->where('matriculas.status','pendente')
-                        ->where('inscricoes.status','regular')
-                        ->groupBy('matriculas.id')
-                        ->havingRaw('count(inscricoes.id) = ?',[1])
-                        ->inRandomOrder()
-                        ->first();
-        $amostra5 = \App\Lancamento::where('matricula', $matricula5->id)->orderbyDesc('parcela')->first();
-        if($amostra5->parcela >5 || abs($amostra5->valor - 20.00) != 00){
-            $erros++;
-            $erro_str = $erro_str . 'Lançamento '.$amostra5->id.' da pessoa '.$amostra5->pessoa. ': Parcelas: '.$amostra5->parcela.'/5 Valor:'.$amostra5->valor.'/20'."\n";
-        }
-       
-        //Amostra 6: matrícula ativa UATI BOLSISTA
-        
-      
-        //Amostra 7: matrícula espera UATI 1 disciplina
-    
-        //Amostra 8: matrícula ativa PID
-        
-        //Amostra 9: matrícula ativa PID Com Bolsa
-        
-        //Amostra 10: matrícula PID em espera
-        
-        //Amostra 11: matrícula PID em espera Com bolsa
-        
-        //Amostra 12: matrícula ativa CE
-        
-        //Amostra 13: matrícula ativa CE Com Bolsa
-        
-        //Amostra 14: matrícula CE em espera
-        
-        //Amostra 15: matrícula CE em espera Com bolsa
        
         $this->assertEquals(0,$erros,$erro_str);
 

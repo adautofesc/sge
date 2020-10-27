082bc04d5430939e8708eb800ae1e117deb202ad
adautofesc <adauto.oliveira@fesc.saocarlos.sp.gov.br>
2020-10-27 10:15:07 -0300

    corrigindo o cadastrar

--- resources/views/turmas/cadastrar.blade.php
+++ resources/views/turmas/cadastrar.blade.php
@@ -332,7 +332,7 @@ $(document).ready(function()
        else {
  
            //AJAX is called.
- 			$.get("{{asset('pedagogico/cursos/listarporprogramajs/')}}"+"/"+name)
+ 			$.get("{{asset('cursos/listarporprogramajs/')}}"+"/"+name)
  				.done(function(data) 
  				{
  					$.each(data, function(key, val){
@@ -383,7 +383,7 @@ $(document).ready(function()
        else {
  
            //AJAX is called.
- 			$.get("{{asset('pedagogico/curso/disciplinas/')}}"+"/"+curso+"/"+name)
+ 			$.get("{{asset('cursos/disciplinas/grade/')}}"+"/"+curso+"/"+name)
  				.done(function(data) 
  				{
  					$.each(data, function(key, val){
@@ -429,7 +429,7 @@ function cursoEscolhido(id,nome){
 				$('#fmodulo').attr('max',data);
 			}
 		});*/
-	$.get("{{asset('pedagogico/curso/disciplinas')}}"+"/"+id)
+	$.get("{{asset('cursos/disciplinas/grade')}}"+"/"+id)
 		.done(function(data) {
 			
 			if(data.length>0){

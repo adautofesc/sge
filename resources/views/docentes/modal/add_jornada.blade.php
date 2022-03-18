34cea7817a3b60ddc4f1b3f767e18f75fc61d71c
Adauto <adauto.oliveita@fesc.com.br>
2022-03-18 16:54:18 -0300

    ajuste add jornada

--- resources/views/docentes/modal/add_jornada.blade.php
+++ resources/views/docentes/modal/add_jornada.blade.php
@@ -23,7 +23,8 @@
                     <div class="col-sm-6"> 
                         <select class="c-select form-control boxed" name="tipo" required>
                             <option>Selecione uma opção</option>
-                            <option value="Aula" title="Utilizado para turmas posteriores ou mudanças de horários">Aula - planejamento</option>
+                            <option value="Aula" title="Utilizado para turmas posteriores ou mudanças de horários">Aula - turma futura</option>
+                            <option value="Coordenação" >HTP</option>
                             <option value="HTP" >HTP</option>
                             <option value="Translado">Translado</option>
                             <option value="Projeto">Projeto</option>

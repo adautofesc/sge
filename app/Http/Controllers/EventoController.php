47dafa6b79b5ffca0ac0b96001e4109844a18af1
Adauto <adauto.oliveita@fesc.com.br>
2023-03-27 14:50:44 -0300

    separação logs parte 1

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -9,11 +9,107 @@ use Illuminate\Http\Request;
 
 class EventoController extends Controller
 {
+
     public function index(Request $r){
+        $mes = collect();
+        if(!isset($r->data))
+            $data = new \DateTimeImmutable();
+        else
+            $data =  \DateTimeImmutable::createFromFormat('Y-m-d',$r->data);
+        
+        //dd($data->format('w'));
+
+        $dias_mes = $this->get_days_count_in_month($data->format('Y'), $data->format('m'));
+
+        //pegar o primeiro dia do mês
+        $primeiro_dia = \DateTimeImmutable::createFromFormat('Y-m-d',$data->format('Y-m-01'));
+        $dia = $primeiro_dia->format('w');;
+        
+        for($i=0;$i<$primeiro_dia->format('w');$i++){
+            $cell = new \stdClass();
+            $cell->id = $i;
+            $cell->weekday = $primeiro_dia->format('w');
+            $cell->class = '';
+            $cell->number = '';
+            $mes->push($cell);
+
+        }
+        
+
+        for($i=0;$i<=$dias_mes;$i++){
+            if($dia == 6)
+                $dia = 0;
+            else
+                $dia++;
+            $cell = new \stdClass();
+            $cell->id = $i+$primeiro_dia->format('w');
+            $cell->class = 'current-month';
+            $cell->weekday = $dia;
+            $cell->number = $i+1;
+            $cell->events = [];
+            $mes->push($cell);
+
+
+
+        }
+
+//dd($mes);
+
+
+        $dia_semana = 0;
+
+       
+
+        
+        
+
+
         $eventos = Evento::where('data_termino','>=',date('Y-m-d'))->orderBy('data_inicio')->get();
-        return $eventos;
+        return view('eventos.index')->with('eventos',$eventos)->with('data',$data)->with('week',0);
 
     }
+
+    
+
+    /**
+     * Numero de dias que o mês possui
+     *
+     * @param [type] $year
+     * @param [type] $month
+     * @return void
+     */
+    public function get_days_count_in_month($year, $month) {
+        return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
+    }
+    public function get_next_month($y, $m) {
+        $y = intval($y);
+        $m = intval($m);
+
+        //***
+        $m++;
+        if ($m % 13 == 0 OR $m > 12) {
+            $y++;
+            $m = 1;
+        }
+
+        return array('y' => $y, 'm' => $m);
+    }
+
+    public function get_prev_month($y, $m) {
+        $y = intval($y);
+        $m = intval($m);
+
+        //***
+        $m--;
+        if ($m <= 0) {
+            $y--;
+            $m = 12;
+        }
+
+        return array('y' => $y, 'm' => $m);
+    }
+
+
     public function filter(){
 
     }
